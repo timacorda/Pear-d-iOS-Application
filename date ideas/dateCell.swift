2d9992e8763127979b51200b36c98479bbf3ecfb
timacorda <tacorda@calpoly.edu>
2015-01-30 23:16:06 -0800

    Added layout items, custom cell.

---
+++ date ideas/dateCell.swift
@@ -0,0 +1,29 @@
+//
+//  dateCell.swift
+//  date ideas
+//
+//  Created by Timothy Acorda on 1/30/15.
+//  Copyright (c) 2015 hackanddev. All rights reserved.
+//
+
+import UIKit
+
+class dateCell: UITableViewCell {
+    
+    @IBOutlet weak var dateImage: UIImageView!
+    @IBOutlet weak var dateName: UILabel!
+    @IBOutlet weak var dateCategory: UILabel!
+    @IBOutlet weak var voteCount: UILabel!
+    @IBOutlet weak var dollarImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
